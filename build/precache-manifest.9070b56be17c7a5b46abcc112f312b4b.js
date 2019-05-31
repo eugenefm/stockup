2f46888141b5ad6f9770d629ffe3f8eb3ee4d973
Eugene Michasiw <eugene@michasiw.com>
2019-05-31 15:16:16 -0400

    deployr build

---
+++ build/precache-manifest.9070b56be17c7a5b46abcc112f312b4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e21c8b241fe371aad4fdf0a45d5acea4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0bb959c71ccefa17195",
+    "url": "/static/css/main.caf0eef0.chunk.css"
+  },
+  {
+    "revision": "874f99c33ef789703993",
+    "url": "/static/js/2.ce75dbb0.chunk.js"
+  },
+  {
+    "revision": "e0bb959c71ccefa17195",
+    "url": "/static/js/main.0e121cde.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "78679e0747540403d4b330d2ae16411c",
+    "url": "/static/media/logo.78679e07.svg"
+  }
+]);
