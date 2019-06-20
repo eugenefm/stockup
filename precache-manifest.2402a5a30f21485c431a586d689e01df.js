b6d9f1e2ceadfd6c9af9aef05212504378312a2e
Eugene Michasiw <eugene@michasiw.com>
2019-06-20 13:24:21 -0400

    Updates

---
+++ precache-manifest.2402a5a30f21485c431a586d689e01df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80108b5bd6757d30960055d347b84c73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c2509012ca8006c6388",
+    "url": "/static/css/main.be7f08dd.chunk.css"
+  },
+  {
+    "revision": "c8a585e3a49204e2b62d",
+    "url": "/static/js/2.d08d1721.chunk.js"
+  },
+  {
+    "revision": "3c2509012ca8006c6388",
+    "url": "/static/js/main.1b671ccd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "78679e0747540403d4b330d2ae16411c",
+    "url": "/static/media/logo.78679e07.svg"
+  }
+]);
